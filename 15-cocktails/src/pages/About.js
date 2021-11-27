05ee9faf9c4664c8ffcf7e331e6457b21d09c8b1
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-11-27 18:07:12 +0100

    project 15 done

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        obcaecati inventore magni repellat, est asperiores totam ipsa saepe,
+        maxime odit illum modi sint qui libero pariatur cumque. Distinctio autem
+        neque adipisci, repellendus itaque veritatis dignissimos. Veritatis
+        magni et velit debitis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
