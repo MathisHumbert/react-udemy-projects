c516e328ed8e848e39be5cf0a7bb489ec2cb576c
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-03 17:38:46 +0100

    refact project 21

--- 21-movie-db/src/SingleMovie.js
+++ 21-movie-db/src/SingleMovie.js
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { API_ENDPOINT } from './context';
+import useFetch from './useFetch';
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const [movie, setMovie] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: '' });
 
-  const fetchMovie = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.Resonse === 'False') {
-      setError({ show: true, msg: data.Error });
-      setLoading(false);
-    } else {
-      setMovie(data);
-      setLoading(false);
-    }
-  };
+  const { isLoading, error, data: movie } = useFetch(`&i=${id}`);
+  // const [movie, setMovie] = useState({});
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState({ show: false, msg: '' });
 
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`);
-  }, [id]);
+  // const fetchMovie = async (url) => {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   if (data.Resonse === 'False') {
+  //     setError({ show: true, msg: data.Error });
+  //     setLoading(false);
+  //   } else {
+  //     setMovie(data);
+  //     setLoading(false);
+  //   }
+  // };
 
-  if (loading) {
+  // useEffect(() => {
+  //   fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  // }, [id]);
+
+  if (isLoading) {
     return <div className="loading"></div>;
   }
 
